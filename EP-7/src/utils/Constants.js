ab1d75124ac47f88cd15d2ab535ec3b51e96d9fb
Manash <manashhalder2212@gmail.com>
2023-12-30 21:06:47 +0530

    Solved Updated Problem in Git and create EP-7 Folder and some Files inside in it

---
+++ EP-7/src/utils/Constants.js
@@ -0,0 +1,5 @@
+export const RestaurantImg =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const HeaderLogo =
+  "https://thumbs.dreamstime.com/b/food-delivery-logo-template-vector-icon-illustration-170869600.jpg";
